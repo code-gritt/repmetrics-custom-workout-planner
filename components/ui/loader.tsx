c9edfbf91b13eda9e926891673a8f3f79ecb7245
Gokul <gokulchandan24@gmail.com>
2025-09-14 03:25:24 +0530

    a

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export const Loader = () => {
   return (
-    <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-primary"></div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+      <div className="relative w-[120px] h-[90px]">
+        <div className="absolute w-6 h-6 bg-orange-500 rounded-sm animate-square1"></div>
+        <div className="absolute w-6 h-6 bg-orange-500 rounded-sm animate-square2"></div>
+        <div className="absolute w-6 h-6 bg-orange-500 rounded-sm animate-square3"></div>
+        <div className="absolute w-6 h-6 bg-orange-500 rounded-sm animate-square4"></div>
+        <div className="absolute w-6 h-6 bg-orange-500 rounded-sm animate-square5"></div>
+      </div>
+    </div>
   );
 };

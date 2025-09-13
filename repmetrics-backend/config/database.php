8450c6f04f905aa07afe393dfcbd3a856376235c
Gokul <gokulchandan24@gmail.com>
2025-09-13 18:36:53 +0530

    a

--- repmetrics-backend/config/database.php
+++ repmetrics-backend/config/database.php
@@ -1,11 +1,11 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
-    'default' => env('DB_CONNECTION', 'pgsql'),
+
+    'default' => 'pgsql',
 
     'connections' => [
+
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => database_path('database.sqlite'),
@@ -15,11 +15,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'laravel',
+            'username' => 'root',
+            'password' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -30,23 +30,26 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => "postgresql://neondb_owner:npg_gRqKP92QEuak@ep-fragrant-art-afb3jab1-pooler.c-2.us-west-2.aws.neon.tech/repmetrics-database?sslmode=require&channel_binding=require&options=endpoint%3Dep-fragrant-art-afb3jab1",
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
-
 
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'localhost',
+            'port' => '1433',
+            'database' => 'laravel',
+            'username' => 'root',
+            'password' => '',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -59,27 +62,21 @@ return [
     ],
 
     'redis' => [
-        'client' => env('REDIS_CLIENT', 'phpredis'),
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel')) . '-database-'),
-            'persistent' => env('REDIS_PERSISTENT', false),
-        ],
+        'client' => 'phpredis',
+
         'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'host' => '127.0.0.1',
+            'password' => null,
+            'port' => 6379,
+            'database' => 0,
         ],
+
         'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'host' => '127.0.0.1',
+            'password' => null,
+            'port' => 6379,
+            'database' => 1,
         ],
     ],
+
 ];
